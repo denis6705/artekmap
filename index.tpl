7851f948c1ac3089e46eac006bfab9bd0054cfb2
Шкабатур Денис Геогриевич <DShkabatur@corp.artek.org>
2018-02-07 15:15:08 +0300

    now with bottle.py

--- index.tpl
+++ index.tpl
@@ -6,7 +6,26 @@
 </head>
 <body>
    <canvas id="canvas" width="1600" height="900"> </canvas>
-   <script src="/static/js/script.js" >
+   <script>
+	window.onload = function() {
+	ctx = document.getElementById("canvas").getContext("2d");
+	map = new Image();
+	map.src = "/static/img/map.jpg";
+	red = new Image();
+	red.src = "/static/img/red.png";
+	green = new Image();
+	green.src = "/static/img/green.png";
+	map.onload = function()
+	{
+		ctx.drawImage(map,0,0,1600,900);
+		ctx.drawImage(red,1209,400,10,10);
+		ctx.drawImage(green,820,414,10,10);
+	}
+	%for key in pings:
+		console.log("{{key}}" + ":" + {{pings[key]}});
+	%end
+		
+}
    </script>
 </body>
 </html>
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from bottle import *
-
+from ping3 import ping
 @get("/static/img/<filepath:re:.*\.(jpg|png|gif|ico|svg)>")
 def img(filepath):
     return static_file(filepath, root="static/img")
@@ -8,8 +8,26 @@ def img(filepath):
 def js(filepath):
     return static_file(filepath, root="static/js")
 
+nodes = {
+	"Хрустальный"   : "172.20.255.10",
+	"Янтарный"      : "172.20.255.11",
+	"ФОК"           : "172.20.255.12",
+	"Морской"       : "172.20.255.20",
+	"ЦДО"           : "172.20.255.30",
+	"Вожатый"       : "172.20.255.40",
+	"Залупа"		: "172.20.20.20"
+}
+
+pings = dict()
+
+for key in nodes:
+	pings[key] = ping(nodes[key], 1)
+	
+	
+
 @route('/')
 def index():
-	return template('index.tpl')
+	return template('index.tpl',pings=pings)
 
 run(host='localhost', port=80)
+
