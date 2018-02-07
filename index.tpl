83c526875ebfbb39bc489be762c09631357f3d31
Шкабатур Денис Геогриевич <DShkabatur@corp.artek.org>
2018-02-07 15:53:00 +0300

    nothing to say

--- index.tpl
+++ index.tpl
@@ -5,21 +5,21 @@
    <title>Карта Артека</title>
 </head>
 <body>
-   <canvas id="canvas" width="1600" height="900"> </canvas>
+   <canvas id="canvas" width="4096" height="2160"> </canvas>
    <script>
 	window.onload = function() {
 	ctx = document.getElementById("canvas").getContext("2d");
 	map = new Image();
-	map.src = "/static/img/map.jpg";
+	map.src = "/static/img/map3.png";
 	red = new Image();
 	red.src = "/static/img/red.png";
 	green = new Image();
 	green.src = "/static/img/green.png";
 	map.onload = function()
 	{
-		ctx.drawImage(map,0,0,1600,900);
-		ctx.drawImage(red,1209,400,10,10);
-		ctx.drawImage(green,820,414,10,10);
+		ctx.drawImage(map,0,0,4096,2160);
+		ctx.drawImage(red,1209,400,30,30);
+		ctx.drawImage(green,820,414,30,30);
 	}
 	%for key in pings:
 		console.log("{{key}}" + ":" + {{pings[key]}});
--- main.py
+++ main.py
@@ -8,25 +8,27 @@ def img(filepath):
 def js(filepath):
     return static_file(filepath, root="static/js")
 
-nodes = {
-	"Хрустальный"   : "172.20.255.10",
-	"Янтарный"      : "172.20.255.11",
-	"ФОК"           : "172.20.255.12",
-	"Морской"       : "172.20.255.20",
-	"ЦДО"           : "172.20.255.30",
-	"Вожатый"       : "172.20.255.40",
-	"Залупа"		: "172.20.20.20"
-}
 
-pings = dict()
-
-for key in nodes:
-	pings[key] = ping(nodes[key], 1)
 	
 	
 
 @route('/')
 def index():
+	nodes = {
+		"Хрустальный"   : "172.20.255.10",
+		"Янтарный"      : "172.20.255.11",
+		"ФОК"           : "172.20.255.12",
+		"Морской"       : "172.20.255.20",
+		"ЦДО"           : "172.20.255.30",
+		"Вожатый"       : "172.20.255.40",
+		"Залупа"		: "172.20.20.20"
+	}
+
+	pings = dict()
+
+	for key in nodes:
+		pings[key] = ping(nodes[key], 1)
+	
 	return template('index.tpl',pings=pings)
 
 run(host='localhost', port=80)
